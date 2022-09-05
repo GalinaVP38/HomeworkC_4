5ef136f79ad9fc441ff0a2f742849b2319e53610
Galina <pgv21@yandex.ru>
2022-09-05 19:15:43 +0300

    saveTask27

---
+++ Seminar4_Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В данном числе сумма цифр = " + GetSumNum(num));
+
+int GetSumNum (int num)
+{
+    int sum = 0;
+    int count = Convert.ToString(num).Length;
+    int num1 = 0;
+    for (int i=0; i<count; i++)
+    {
+        num1=num-(num%10);
+        sum=sum+(num-num1);
+        num=num/10;
+    }
+    return sum;
+}
