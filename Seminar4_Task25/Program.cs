d1d631a466e63840f84b2ef1e39b54f60038186b
Galina <pgv21@yandex.ru>
2022-09-05 18:32:35 +0300

    saveTask25

---
+++ Seminar4_Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа ( А и В)
+// и возводит число А в натуральную степень В.
+
+Console.WriteLine("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("А в степени В: "+GetExp(numA,numB));
+
+double GetExp (int numA, int numB)
+{
+    double result=Math.Pow(numA,numB);
+    return result; 
+}
