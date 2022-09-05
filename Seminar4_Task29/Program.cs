648916a9962b166586b59f5e6b98fb6e85ddb1df
Galina <pgv21@yandex.ru>
2022-09-05 20:25:48 +0300

    saveTask29

---
+++ Seminar4_Task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, кот. задает массив из 8 эллементов и выводит их на экран
+int [] array = new int[8];
+
+void FillArray (int [] compilation)
+{
+    int lenght = compilation.Length;
+    int index = 0;
+    while (index<lenght)
+    {
+        compilation[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray (int [] compilation)
+{
+    int count = compilation.Length;
+    int pos = 0;
+    while (pos<count)
+    {
+        Console.WriteLine(compilation[pos]);
+        pos++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
